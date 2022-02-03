5123c0380eae7b3525090fb2acaed7058130ae18
Walter Caffiero <w.caffiero@entando.com>
2022-02-03 18:18:18 +0100

    Better init error management and fix configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,60 @@
 #!/bin/sh
 
-# Before PostgreSQL can function correctly, the database cluster must be initialized:
-/usr/pgsql-14/bin/initdb -D /var/lib/pgsql/data
+PGCTL="/usr/pgsql-14/bin/pg_ctl"
 
-# internal start of server in order to allow set-up using psql-client
-# does not listen on external TCP/IP and waits until start finishes
-/usr/pgsql-14/bin/pg_ctl -D "/var/lib/pgsql/data" -o "-c listen_addresses='*'" -w start
+if [ -f "$PGDATA/initialized" ]; then
+  echo "Database already initialized" 1>&2
+else
+  # REQUIRED VARS CHECK
+  [[ -z "${POSTGRESQL_DATABASE}" || "${POSTGRESQL_DATABASE}" == *'"'* ]] && {
+    echo "Invalid POSTGRESQL_DATABASE" 1>&2
+    exit 1
+  }
 
-# create a user or role
-psql -d postgres -c "CREATE USER ${POSTGRESQL_USER} WITH PASSWORD '${POSTGRESQL_PASSWORD}';" 
+  [[ -z "${POSTGRESQL_USER}" || "${POSTGRESQL_USER}" == *'"'* ]] && {
+    echo "Invalid POSTGRESQL_USER" 1>&2
+    exit 1
+  }
 
-# create database 
-psql -v ON_ERROR_STOP=1 -d postgres -c "CREATE DATABASE ${POSTGRESQL_DATABASE} OWNER '${POSTGRESQL_USER}';"
+  [[ -z "${POSTGRESQL_PASSWORD}" || "${POSTGRESQL_PASSWORD}" == *'"'* ]] && {
+    echo "Invalid POSTGRESQL_PASSWORD" 1>&2
+    exit 1
+  }
 
-# stop internal postgres server
-/usr/pgsql-14/bin/pg_ctl -D "/var/lib/pgsql/data" -m fast -w stop -l /var/lib/pgsql/log/stdout
+  [[ "${POSTGRESQL_ADMIN_PASSWORD}" == *"'"* ]] && {
+    echo "Invalid POSTGRESQL_ADMIN_PASSWORD" 1>&2
+    exit 1
+  }
 
-sed -i 's/logging_collector = on/logging_collector = off/g' /var/lib/pgsql/data/postgresql.conf
+  # Before PostgreSQL can function correctly, the database cluster must be initialized:
+  [ ! -d "$PGDATA" ] && /usr/pgsql-14/bin/initdb -D "$PGDATA"
 
+  # internal start of server in order to allow set-up using psql-client
+  # does not listen on external TCP/IP and waits until start finishes
+  $PGCTL -D "$PGDATA" -o "-c listen_addresses='*'" -w start
+
+  # CONFIGURATION CHANGES
+  sed -i -e 's/logging_collector = on/logging_collector = off/g' -e "s/listen_address.*/listen_addresses='*'/g" "$PGDATA/postgresql.conf"
+  echo "host all  all    0.0.0.0/0  md5" >> "$PGDATA/pg_hba.conf" && \
+  echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
+  $PGCTL reload
+
+  # CREATE USER OR ROLE
+  psql -d postgres -c "CREATE USER \"${POSTGRESQL_USER}\" WITH PASSWORD '${POSTGRESQL_PASSWORD}';" 
+
+  # CREATE DATABASE
+  echo "Creating database: ${POSTGRESQL_DATABASE}"
+
+  psql -v ON_ERROR_STOP=1 -d postgres -c "CREATE DATABASE \"${POSTGRESQL_DATABASE}\" OWNER '${POSTGRESQL_USER}';"
+  if [ -n "$POSTGRESQL_ADMIN_PASSWORD" ]; then
+    psql -v ON_ERROR_STOP=1 -d postgres -c "ALTER USER postgres WITH ENCRYPTED PASSWORD '${POSTGRESQL_ADMIN_PASSWORD}';"
+  fi
+
+  # STOP SERVER
+  $PGCTL -D "$PGDATA" -m fast -w stop
+fi
+
+touch "$PGDATA/initialized"
+
+#..
 exec "$@"
