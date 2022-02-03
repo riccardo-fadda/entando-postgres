d68ae8e61844c80987f6ebbed78bad2de44fba2e
w-caffiero-entando <63636347+w-caffiero-entando@users.noreply.github.com>
2022-02-03 16:23:06 +0100

    Quoting and injection checks in create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-POSTGRES="psql --username '${POSTGRESQL_USER}'"
+POSTGRES="psql --username \"${POSTGRESQL_USER}\""
 
 echo "Creating database: ${POSTGRESQL_DATABASE}"
 
+[[ "${POSTGRESQL_DATABASE}" == *'"'* ]] && {
+  echo "Invalid database name" 1>&2
+  exit 1
+}
+
+[[ "${POSTGRESQL_USER}" == *'"'* ]] && {
+  echo "Invalid user name" 1>&2
+  exit 1
+}
+
 $POSTGRES <<EOSQL
-CREATE DATABASE ${POSTGRESQL_DATABASE} OWNER ${POSTGRESQL_USER};
+CREATE DATABASE "${POSTGRESQL_DATABASE}" OWNER "${POSTGRESQL_USER}";
 EOSQL
